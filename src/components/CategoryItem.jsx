aa43345282512fd04c8934062b6a6989f4c81472
Ezequiel <ezequieldiazpierri@gmail.com>
2022-10-31 02:50:22 -0300

    Homepage

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,10 +1,48 @@
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
-const Container = styled.div``;
-const Image = styled.img``;
-const Info = styled.div``;
-const Title = styled.h1``;
-const Button = styled.button``;
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  ${mobile({ height: "20vh" })}
+`;
+
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+  -webkit-text-stroke: 1px black;
+  font-size: 50px;
+`;
+
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 20px;
+`;
 
 const CategoryItem = ({ item }) => {
   return (
@@ -12,7 +50,7 @@ const CategoryItem = ({ item }) => {
       <Image src={item.img} />
       <Info>
         <Title>{item.title}</Title>
-        <Button>{item.title}</Button>
+        <Button>COMPRAR</Button>
       </Info>
     </Container>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
-import Slider from "../components/Slider";
 import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
@@ -11,6 +13,9 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Categories />
+      <Products />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
